neurodebian/psychtoolbox-3

commit 1c93d01a4e44aced41eaa77721319c60b78bb581

Screen/Linux: Avoid redundant RandR output props query in non-VRR mode.
Avoid needless call to PsychOSGetOutputProps() in OpenWindow path
if vrr is not requested. This prevents an 'invalid outputIndex no
such output' abort on Vulkan multi-window configurations.

The inability to find RandR outputs that are logically disconnected
if a RandR output is leased out to Vulkan remains a big pita. We
can work around it to some degree with hacks, but i think i soon
will have to redesign RandR output enumeration in Screen(), as this
hackery has serious limits. Atm. we can't call any function that tries
to find a RandR output based on outputIndex, because we find RandR
outputs by mapping output indices to RandR crtc's and then find the
matching RandR output for a RandR crtc, an approach that fails badly
if no RandR crtc is associated with a logically disabled RandR output,
because the RandR output has been leased out to a direct display client
like our Vulkan driver.

Anyway, this hack makes the following commits (barely) work ok'ish,
good enough to get some testing done.

PsychSourceGL/Source/Linux/Screen/PsychWindowGlue.c
@@ -1423,10 +1423,11 @@ psych_bool PsychOSOpenOnscreenWindow(PsychScreenSettingsType *screenSettings, Ps
 
         // Find primary output for check in 'output':
         PsychUnlockDisplay();
-        output_name = PsychOSGetOutputProps(screenSettings->screenNumber, 0, FALSE, NULL, NULL, (unsigned long *) &output);
+        if (vrr_wanted)
+            output_name = PsychOSGetOutputProps(screenSettings->screenNumber, 0, FALSE, NULL, NULL, (unsigned long *) &output);
         PsychLockDisplay();
 
-        if (vrr_supported_atom &&
+        if (vrr_supported_atom && vrr_wanted &&
             (XRRGetOutputProperty(dpy, output, vrr_supported_atom, 0, 4, False, False, None, &actual_type, &actual_format, &nitems, &bytes_after, &prop) == Success) &&
             (actual_type == XA_INTEGER) && (nitems == 1) && (actual_format == 32)) {
             // printf("%s : %p %ld %d \n", output_name, prop, *((long *) prop), *((long *) prop) > 0);

commit 0b27d1b90b4fac9c0321a30f0782e0b7f9aa4d4f

Screen/Linux: Fixup vrr RandR output query commit.
Commit 1c93d01a4e44aced41eaa77721319c60b78bb581 introduced
a little error in that it may not properly disable VRR mode
if VRR mode shall not be used/disabled. As vrr_supported=FALSE
now whenever vrr_wanted is FALSE, we skipped some code that
must run whenever vrr_supported should be true but isn't anymore,
iow. code to actively disable VRR if it is supported by not
wanted.

PsychSourceGL/Source/Linux/Screen/PsychWindowGlue.c
@@ -1523,7 +1523,7 @@ psych_bool PsychOSOpenOnscreenWindow(PsychScreenSettingsType *screenSettings, Ps
         Atom vrr_atom;
 
         // Running on Mesa 19.0.0 or later with DRI3/Present support for VRR?
-        if (vrr_supported && (mesaversion[0] >= 19)) {
+        if (mesaversion[0] >= 19) {
             // Yes. Perform an OpenGL bufferswap, so Mesa can set up VRR state according to its opinion and
             // system settings and then we can override it with our own opinion:
             glClearColor(0, 0, 0, 0);